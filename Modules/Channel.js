2f18ec360acb823d00595ace366bc0f7dbf79b06
ansonfoong <afoong@albany.edu>
2019-05-28 18:05:31 -0400

    Added several properties to the Channel object

--- Modules/Channel.js
+++ Modules/Channel.js
@@ -2,20 +2,31 @@ const RequestHandler = require('../Requests/RequestHandler');
 
 class Channel {
 
-    constructor(id)
+    constructor(id, name, type, guild_id, topic)
     {
         this._id = id;
+        this._name = name;
+        this._type = type;
+        this._guild_id = guild_id;
+        this._topic = topic;
         this.request = new RequestHandler();
     }
 
     set id(id) { this._id = id; }
     get id() { return this._id; }
 
-    init()
-    {
-        
-    }
+    set name(name) { this._name = name; }
+    get name() { return this._name; }
 
+    set type(type) { this._type = type; }
+    get type() { return this._type; }
+
+    set guild_id(id) { this._guild_id = id; }
+    get guild_id() { return this._guild_id; }
+
+    set topic(topic) { this._topic = topic; }
+    get topic() { return this._topic; }
+    
     send(message, token)
     {
         this.request.createMessage(message, this._id, token);
