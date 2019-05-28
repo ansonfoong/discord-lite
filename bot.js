8ab5538f93975cba6f989132c73c46d7d9ac9d65
ansonfoong <afoong@albany.edu>
2019-05-28 14:20:20 -0400

    Setting up config files and hiding bot token...

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const DiscordLite = require('./discordlite');
 const client =  new DiscordLite.Client();
-const token = "NTc4MTUyMjA2NjI1MTQ0ODQy.XO116g.rF1hlon8zA4nAsp0Amr1CbnFTbo"
-client.login(token);
+const TOKEN = require('./config/config.json').TOKEN;
+
+client.login(TOKEN);
 
 client.on('ready', (readyMsg) => {
     console.log(readyMsg);
